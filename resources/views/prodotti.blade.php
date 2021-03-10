35547f5bf173d802a4e9c374be581fa10c69bf71
ilariamammucari <>
2021-03-10 15:04:58 +0100

    prova pagina prodotti

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,3 +1,12 @@
 @extends('layout.app')
 
 @section('titolo', 'Prodotti')
+@section('content')
+    <div class="box-card">
+        <div class="card">
+            @foreach ($formati as $formato)
+                <img src="{{ $formato['src'] }}" alt="formato-pasta">
+            @endforeach
+        </div>
+    </div>
+@endsection
