3de393062b6cdd578b9f7c1643a4db86cad05ead
ilariamammucari <>
2021-03-11 13:42:08 +0100

    creo file dei db che mi servono

---
+++ config/notizie.php
@@ -0,0 +1,23 @@
+<?php 
+    return [
+        [
+            'img' => 'https://www.lamolisana.it/wp-content/uploads/2021/01/gemelli-insieme-in-salute-header.jpg',
+            'data' => '10/03/21',
+            'descrizione' => '“Geometrie del Molise” di Nicola Vizzarri è il piatto vincitore del contest “Migliore chef d’Italia”'
+        ],
+        [
+            'img' => 'https://www.lamolisana.it/wp-content/uploads/2021/01/gemelli-insieme-in-salute-header.jpg',
+            'data' => '08/03/21',
+            'descrizione' => 'Proseguono i webinar organizzati da Gemelli Molise, domani si parlerà dei tumori femminili'
+        ],
+        [
+            'img' => 'https://www.lamolisana.it/wp-content/uploads/2021/01/gemelli-insieme-in-salute-header.jpg',
+            'data' => '08/03/21',
+            'descrizione' => '#solodalcuore, Rossella Ferro testimonial del progetto benefico'
+        ],
+        [
+            'img' => 'https://www.lamolisana.it/wp-content/uploads/2021/01/gemelli-insieme-in-salute-header.jpg',
+            'data' => '08/03/21',
+            'descrizione' => 'La Molisana regala 240 chili di pasta a 16 comuni della regione'
+        ]
+    ];
